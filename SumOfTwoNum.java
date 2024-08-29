8e91331fc3e69b4b6535e6a419d6d338b75fb44d
Abhishek <ak7320005612@gmail.com>
2024-08-29 13:55:23 +0530

    Add Inital Files.

---
+++ SumOfTwoNum.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class SumOfTwoNum {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter First number :");
+        int num1 = sc.nextInt();
+        System.out.print("Enter second number :");
+        int num2 = sc.nextInt();
+
+        System.out.println("Sum is "+(num1+num2));
+    }
+}
